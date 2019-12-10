1cce575fff0eb353975b9f0a06a5a45f76bf9a3f
theredbart <bartosz.wrobel.yt@gmail.com>
2019-12-10 18:01:11 +0100

    began package realisation

--- include/package.hpp
+++ include/package.hpp
@@ -1,6 +1,22 @@
-
-
 #ifndef NETWORK_SIM_PACKAGE_HPP
 #define NETWORK_SIM_PACKAGE_HPP
+#include "types.hpp"
+#include <cstdlib>
+#include <set>
+
+static std::set<ElementID> assigned_IDs;
+static std::set<ElementID> freed_IDs;
+
+class Package{
+
+private:
+    ElementID id;
+
+public:
+    Package() {}
+    const ElementID get_id() {return id;}
+    Package& operator=(Package&&);
+
+};
 
 #endif //NETWORK_SIM_PACKAGE_HPP
--- include/types.hpp
+++ include/types.hpp
@@ -1,6 +1,6 @@
-
-
 #ifndef NETWORK_SIM_TYPES_HPP
 #define NETWORK_SIM_TYPES_HPP
+
+using ElementID = unsigned int;
 
 #endif //NETWORK_SIM_TYPES_HPP
