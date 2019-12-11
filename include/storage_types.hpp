424639d480f1a09c8625dca1b11627754aa3f10a
bwrobel <bartosz.wrobel.yt@gmail.com>
2019-12-11 19:57:02 +0100

    structure_types skeleton done, added includes to main.cpp

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,6 +1,33 @@
-
-
 #ifndef NETWORK_SIM_STORAGE_TYPES_HPP
 #define NETWORK_SIM_STORAGE_TYPES_HPP
+
+#include "types.hpp"
+#include "package.hpp"
+
+enum class PackageQueueType { FIFO, LIFO };
+
+class IPackageStockpile{
+
+public:
+    virtual const void push(Package&&) = 0;
+    virtual const bool empty() = 0;
+    virtual const std::size_t size() = 0;
+
+};
+
+class IPackageQueue{
+
+public:
+    virtual Package pop() = 0;
+    virtual const PackageQueueType get_queue_type() = 0;
+
+};
+
+class PackageQueue{
+
+public:
+    PackageQueueType PackageQueue;
+
+};
 
 #endif //NETWORK_SIM_STORAGE_TYPES_HPP
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "include/storage_types.hpp"
+#include "include/types.hpp"
+#include "include/package.hpp"
 int main(){
 
     return 0;
